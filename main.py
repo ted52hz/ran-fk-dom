ce4a922400fd919233c805e037b3d8582bedb2e0
Khoa Huynh-Trong <khoaht.uit@gmail.com>
2024-04-04 14:51:23 +0700

    re-upload

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def read_rss(url):
     for entry in feed.entries:
         match = re.search(r'</a>([^<]*)$', entry.summary, re.DOTALL)
         text_between_a_and_brackets = match.group(1).strip()
-        write += "- " + text_between_a_and_brackets
+        write += "- ." + text_between_a_and_brackets + "\n"
     return write
 
 
@@ -44,13 +44,13 @@ md.write("# Tuoi Tre RSS \n")
 md.write("\n")
 md.write('## Giáo dục \n')
 md.write(edu_writer)
-md.write("![Edu](.Edu.png)")
+md.write("![Edu](Edu.png)")
 md.write("\n")
 md.write('## Thời Sự \n')
 md.write(news_writer)
-md.write("![New](.News.png)")
+md.write("![New](News.png)")
 md.write("\n")
 md.write('## Nhịp sống số \n')
 md.write(life_writer)
-md.write("![life](.Life.png)")
+md.write("![life](Life.png)")
 md.write("\n")
